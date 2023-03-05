ec455c6b1e530cb2d8d4a9ce771ca3b04618c067
Oleh Honchar <ooollleeeg@gmail.com>
2023-03-05 05:14:40 +0200

    add hw-2 task

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
+
+function App() {
+  return (
+    <>
+      <RegistrationScreen />
+      <LoginScreen />
+    </>
+  );
+}
+
+export default App;
